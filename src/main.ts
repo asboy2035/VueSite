e3dfd2c57d2029b2065132dc1cd6e693e0d153c2
ash <ash@asboy2035.com>
2025-04-03 19:17:22 +0200

    move styles to public

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createHead } from '@vueuse/head'
 import { router } from './router'
-import './style.css'
 
 const app = createApp(App)
 const head = createHead()
