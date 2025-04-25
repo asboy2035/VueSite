e963065bcb4485c60785317030f31d7e1f459aa0
ash <ash@asboy2035.com>
2025-04-25 12:38:05 +0800

    add support notification

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,21 @@
     <router-view />
   </transitionable>
   <transition-element ref="cover" />
+  <banner-notification>
+    <h-stack>
+      <v-stack>
+        <h3 class="noMargins">Consider Supporting!</h3>
+        <p class="noMargins">Support me on Ko-fi to help development :)</p>
+      </v-stack>
+
+      <a href="https://ko-fi.com/asboy2035">
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><path fill="currentColor" d="M31.844 11.932c-1.032-5.448-6.48-6.125-6.48-6.125H.964C.156 5.807.057 6.87.057 6.87S-.052 16.637.03 22.637c.22 3.228 3.448 3.561 3.448 3.561s11.021-.031 15.953-.067c3.251-.568 3.579-3.423 3.541-4.98c5.808.323 9.896-3.776 8.871-9.219zm-14.751 4.683c-1.661 1.932-5.348 5.297-5.348 5.297s-.161.161-.417.031c-.099-.073-.14-.12-.14-.12c-.595-.588-4.491-4.063-5.381-5.271c-.943-1.287-1.385-3.599-.119-4.948c1.265-1.344 4.005-1.448 5.817.541c0 0 2.083-2.375 4.625-1.281c2.536 1.095 2.443 4.016.963 5.751m8.23.636c-1.24.156-2.244.036-2.244.036V9.714h2.359s2.631.735 2.631 3.516c0 2.552-1.313 3.557-2.745 4.021z"/></svg>
+          Ko-fi
+        </button>
+      </a>
+    </h-stack>
+  </banner-notification>
   <modal v-if="showDomainTip">
     <h1>You're on the old domain!</h1>
     <p>Access this site at asboy2035.com for a cleaner link!</p>
@@ -30,6 +45,8 @@
   import Modal from "@/components/utils/Modal.vue";
   import Spacer from "@/components/utils/Spacer.vue";
   import HStack from "@/components/layout/HStack.vue";
+  import BannerNotification from "@/components/utils/BannerNotification.vue";
+  import VStack from "@/components/layout/VStack.vue";
 
   const showDomainTip = ref(false)
   const redirectLink = `https://asboy2035.com${location.pathname}${location.search}${location.hash}`;
---
+++ src/components/utils/BannerNotification.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+  import Card from "@/components/layout/Card.vue"
+</script>
+
+<template>
+  <div class="notificationContainer">
+    <card class="bannerNotification">
+      <slot />
+    </card>
+  </div>
+</template>
+
+<style scoped>
+  .notificationContainer {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 6;
+    top: 1rem;
+    left: 0;
+    width: 100vw;
+    height: fit-content;
+
+    background: transparent;
+    border-radius: 0;
+  }
+
+  .bannerNotification {
+    padding: 0.75rem 1rem;
+    width: fit-content;
+    transform: translateY(calc(-100% - 1rem));
+    animation: notification 3s ease forwards;
+    animation-delay: 1s;
+    border-radius: 1.5rem;
+  }
+
+  .bannerNotification:hover {
+    animation: none;
+    transform: translateY(0%);
+  }
+
+  @keyframes notification {
+    0% {
+      transform: translateY(calc(-100% - 1rem));
+      scale: 0.9;
+      filter: blur(1rem);
+    }
+
+    15% {
+      transform: translateY(0%);
+      scale: 1;
+      filter: none;
+    }
+
+    85% {
+      transform: translateY(0%);
+      scale: 1;
+      filter: none;
+    }
+
+    99% {
+      transform: translateY(calc(-100% - 1rem));
+      scale: 0.9;
+      filter: blur(1rem);
+      display: none;
+    }
+  }
+</style>
