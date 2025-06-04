c563623d5487fb311c39de18cf2a07b0b1f1418a
ash <ash@asboy2035.com>
2025-06-04 17:47:49 +0200

    add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from "@/views/HomeView.vue";
-import LinksView from "@/views/LinksView.vue";
-import AppsView from "../views/AppsView.vue";
-import AppPage from "../views/AppPage.vue";
-import GoodiesView from "@/views/GoodiesView.vue";
-import PlaylistPage from "@/views/PlaylistPage.vue";
-import CharCardMaker from "@/views/CharCardMaker.vue";
-import RickrollView from "@/views/RickrollView.vue";
+import HomeView from "@/views/HomeView.vue"
+import LinksView from "@/views/LinksView.vue"
+import AppsView from "../views/AppsView.vue"
+import AppPage from "../views/AppPage.vue"
+import GoodiesView from "@/views/GoodiesView.vue"
+import PlaylistPage from "@/views/PlaylistPage.vue"
+import CharCardMaker from "@/views/CharCardMaker.vue"
+import RickrollView from "@/views/RickrollView.vue"
+import NotFound from "@/views/NotFound.vue"
 
 const routes = [
   { path: '/', component: HomeView },
@@ -17,7 +18,8 @@ const routes = [
   { path: '/playlists/:slug', component: PlaylistPage },
   { path: '/goodies', component: GoodiesView },
   { path: '/create/charcard', component: CharCardMaker  },
-  { path: '/tests/audio', component: RickrollView }
+  { path: '/tests/audio', component: RickrollView },
+  { path: '/:pathMatch(.*)*', component: NotFound }
 ]
 
 export const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+  import Grid from "@/components/layout/Grid.vue"
+  import Card from "@/components/layout/Card.vue"
+  import CardTitle from "@/components/utils/CardTitle.vue"
+  import AboutCard from "@/components/home/cards/AboutCard.vue"
+  import BottomFooter from "@/components/premade/BottomFooter.vue"
+  import Navbar from "@/components/premade/navbar/Navbar.vue"
+  import AboutHeader from "@/components/home/AboutHeader.vue"
+  import AboutSidebar from "@/components/home/AboutSidebar.vue"
+</script>
+
+<template>
+  <div class="navigationView">
+    <about-sidebar />
+    <div class="contentView">
+      <div class="content spaced">
+        <about-header />
+        <grid class="spaced">
+          <card class="attention topZ">
+            <card-title
+              title="This page doesn't exist!"
+              icon="solar:danger-triangle-line-duotone"
+            />
+
+            <p>You may return to a valid page using the navbar or the about card's links.</p>
+          </card>
+
+          <about-card />
+        </grid>
+        <bottom-footer />
+        <navbar />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+  .topZ
+    z-index: 10
+</style>
